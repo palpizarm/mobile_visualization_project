27c8c4f6b07f47254881b68a7945b526f684ea2f
palpizarm <pabloalpizar99@gmail.com>
2020-04-15 19:43:11 -0600

    [improved]change icon launch and the name of app

--- mobile_visualization_project/lib/main.dart
+++ mobile_visualization_project/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Data Visualization',
+      title: 'Chart View',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
