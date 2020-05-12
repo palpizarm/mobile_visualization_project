5d14bb024fd5ac73daeb23ab516f2e9dd8255734
palpizarm <pabloalpizar99@gmail.com>
2020-05-12 05:32:51 -1200

    [new] floating button with app info

--- mobile_visualization_project/lib/homepage.dart
+++ mobile_visualization_project/lib/homepage.dart
@@ -22,12 +22,6 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Color(0xff0b906c),
-          leading: IconButton(icon: Icon(Icons.menu),
-            tooltip: 'Menu',
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => HomePage()));
-            },),
           title:
           Text(
             'Chart View',
@@ -36,6 +30,53 @@ class _HomePageState extends State<HomePage> {
               fontSize: 24,
             ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(FontAwesomeIcons.infoCircle),
+            backgroundColor: Color(0xff0b906c),
+            onPressed: () {
+              showGeneralDialog(
+                barrierLabel: "info",
+                barrierDismissible: true,
+                barrierColor: Colors.black.withOpacity(0.5),
+                transitionDuration: Duration(milliseconds: 500),
+                context: context,
+                pageBuilder: (_, __, ___) {
+                  return Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: 300,
+                      child: SizedBox.expand(child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Visualizacion de la informacion \n v.1.0 \n Pablo Alpizar Monge \n TEC",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              decoration: TextDecoration.none,
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                      ),
+                      ),
+                      margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                    ),
+                  );
+                },
+                transitionBuilder: (_, anim, __, child) {
+                  return SlideTransition(
+                    position: Tween(begin: Offset(0, 1), end: Offset(0, 0))
+                        .animate(anim),
+                    child: child,
+                  );
+                },
+              );
+            }
         ),
         body: Container(
           child: StaggeredGridView.count(
@@ -46,7 +87,9 @@ class _HomePageState extends State<HomePage> {
               Padding(
                 padding: EdgeInsets.all(8.0),
                 child: RaisedButton(
-                  elevation: 14.0,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  elevation: 5.0,
                   onPressed: () {
                     Navigator.push(context,
                         MaterialPageRoute(
@@ -66,7 +109,7 @@ class _HomePageState extends State<HomePage> {
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),),
-                      Image.asset('assets/images/bubble.jpg'),
+                      Image.asset('assets/images/bubble.jpg', height: 380,),
                     ],
                   ),
                 ),
@@ -74,6 +117,8 @@ class _HomePageState extends State<HomePage> {
               Padding(
                 padding: EdgeInsets.all(8.0),
                 child: RaisedButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
                   elevation: 14.0,
                   onPressed: () {
                     Navigator.push(context,
@@ -88,7 +133,7 @@ class _HomePageState extends State<HomePage> {
                     children: <Widget>[
                       Text('\n\n'),
                       Image.asset('assets/images/pie.jpg'),
-                      Text('\nTile of the chart',
+                      Text('\nTotal de personas con discapacidad segun tipo discapacidad',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.green,
@@ -102,6 +147,8 @@ class _HomePageState extends State<HomePage> {
               Padding(
                 padding: EdgeInsets.all(8.0),
                 child: RaisedButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
                   elevation: 14.0,
                   onPressed: () {
                     Navigator.push(context,
@@ -116,7 +163,7 @@ class _HomePageState extends State<HomePage> {
                     children: <Widget>[
                       Text('\n\n'),
                       Image.asset('assets/images/bar.jpg'),
-                      Text('\nTile of the chart',
+                      Text('\nPoblación con dicapacidad por edad segun tipo de discapacidad',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.green,
@@ -130,8 +177,8 @@ class _HomePageState extends State<HomePage> {
             ],
             staggeredTiles: [
               StaggeredTile.extent(4, 500.0),
-              StaggeredTile.extent(2, 350.0),
-              StaggeredTile.extent(2, 350.0),
+              StaggeredTile.extent(2, 400.0),
+              StaggeredTile.extent(2, 400.0),
             ],
           ),
         ),
